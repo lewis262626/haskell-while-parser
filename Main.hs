0c3f69bd3a61189eb705a72089a20ff2d1d999b6
Lewis Robbins <lewis.robbins2+git@gmail.com>
2021-02-28 20:17:47 +0000

    parser combinators for identifier and boolean

--- Main.hs
+++ Main.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import Text.Parsec
+
+import Data.Char (isAlpha)
+
+parseBool :: Parsec String () Bool
+parseBool = do
+    value <- (string "true") <|> (string "false")
+    return $ chooseVal value
+    where chooseVal x = case x of
+                            "true" -> True
+                            "false" -> False
+
+parseAlphaChar :: Parsec String () Char
+parseAlphaChar = satisfy (isAlpha)
+
+identifier :: Parsec String () String
+identifier = (:) <$> parseAlphaChar <*> many1 anyChar
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
